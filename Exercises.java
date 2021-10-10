eed15840e9dfeab5758ccbd8a1ad4fd7bc9992df
inesa-vasserman <60776749+inesa-vasserman@users.noreply.github.com>
2021-10-10 21:08:15 +0300

    Homework 3

---
+++ Exercises.java
@@ -0,0 +1,58 @@
+package com.hillel.hw3;
+
+import java.util.Arrays;
+
+public class Exercises {
+    public static void main(String[] args) {
+    }
+
+//    2) Задан массив случаных чисел типа int в котором есть положительные и отрицательные числа.
+//    Написать метод который вернет массив квадратов этих чисел, отсортированных по возрастанию.
+
+    public static int[] getSortPower(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) Math.pow(arr[i], 2);
+        }
+        Arrays.sort(arr, 0, arr.length);
+        return arr;
+    }
+
+//    4*) Написать метод у которого 2 входящих параметра типа String.
+//    Метод должен вернуть true если путем перестановки символов можно из первой строки получить вторую. Например:
+//            "asdf", "fdsa" => true
+//            "asdf", "asdfg => false
+//            "aab", "bba" => false
+
+    public boolean chechEquals(String firstLine, String secondLine) {
+        char[] arrayOfElements = firstLine.toCharArray();
+        char[] arrayCheckElements = secondLine.toCharArray();
+        if (arrayOfElements.length == arrayCheckElements.length) {
+            StringBuilder sbElements = new StringBuilder();
+            sbElements.append(firstLine);
+            StringBuilder sbCheckElements = new StringBuilder();
+            sbCheckElements.append(secondLine);
+            int lengthArray = arrayCheckElements.length;
+            while (lengthArray != 0) {
+                for (int j = 0; j < arrayCheckElements.length; j++) {
+                    if (arrayCheckElements[j] == arrayOfElements[0]) {
+                        sbElements.deleteCharAt(0);
+                        arrayOfElements = sbElements.toString().toCharArray();
+                        sbCheckElements.deleteCharAt(j);
+                        arrayCheckElements = sbCheckElements.toString().toCharArray();
+
+                    }
+
+
+                }
+                lengthArray--;
+            }
+        }
+        if (arrayCheckElements.length == 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+
---
+++ exercise3.java
@@ -0,0 +1,46 @@
+package com.hillel.hw3;
+
+//Задана строка. Вывести все перестановки этой строки.
+//        Например для строки "abc":
+//        abc, acb, bac, bca, cab, cba
+
+public class exercise3 {
+    public static void main(String[] args) {
+    }
+
+    public static void getVariations(String elements) {
+        String[] arr = elements.split("");
+        prepareElements(arr, 0);
+    }
+
+    private static void prepareElements(String[] arrElements, int i) {
+        if (i == arrElements.length - 1) {
+            getResults(arrElements);
+        } else {
+            for (int j = i; j < arrElements.length; j++) {
+                changeElements(arrElements, i, j);
+                prepareElements(arrElements, i + 1);
+                changeElements(arrElements, i, j);
+            }
+        }
+    }
+
+
+    static void changeElements(String[] arr, int index1, int index2) {
+        String tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+    private static void getResults(String[] pa) {
+        StringBuffer s = new StringBuffer();
+
+        for (String a : pa) {
+            s.append(a);
+        }
+
+        s.substring(0, s.length() - 2);
+
+        System.out.print(s + " ");
+    }
+}
