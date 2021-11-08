f3023a0010707d65e50938c0313b4ce07903b5c9
inesa-vasserman <inna.kiwi.a@gmail.com>
2021-11-08 23:08:01 +0200

    First notes

--- Homework11/homework11/Main.java
+++ Homework11/homework11/Main.java
@@ -1,29 +1,32 @@
 package Homework11.homework11;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
- public static void main(String[][] strings) {
+    public static void main(String[] args) {
      ArrayList<String> people = new ArrayList<String>();
+     String[] arrayOfStrings = {"mama", "dad", "father", "dad", "dad", "string", "uncle", "vegans", "sorted", "cat", "dog", "apple", "dad"};
+     String str = "dad";
+     
+     List<String> listOfStrings = Arrays.asList(arrayOfStrings);
+     people.addAll(listOfStrings);
+        findOccurrence(people);
 
 
  }
 
- public void addToList(ArrayList arrayList, String str) {
-     arrayList.add(str);
- }
 //1. Создать метод countOccurance, принимающий на вход строковый список в качестве параметра и произвольную строку.
 // Список заполнен произвольными словами 10-20 штук, которые могут повторяться в неограниченном количестве.
 // Посчитать сколько раз встречается переданная строка в качестве второго аргумента.
 
-    public static void countOccurrence(ArrayList arrayList, String str) {
+    public static int countOccurrence(ArrayList arrayList, String str) {
      int counter = 0;
-     if (arrayList.contains(str)) {
+     while (arrayList.contains(str)) {
          arrayList.remove(str);
          counter += 1;
      }
-
+        return counter;
     }
 //
 //  2. Создать метод toList, принимающий на вход целочисленный массив произвольной длины.
@@ -31,9 +34,24 @@ public class Main {
 //  Например:
 //    Было Array [1, 2, 3]
 //    Стало List [1, 2, 3]
+
+    public static List toList(int[] arr) {
+        List<Integer> arrToList = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        return arrToList;
+    }
 //
 //   3. Создать метод findUnique, принимающий на вход числовой список состоящий из произвольных значений,
 //   которые могут повторяться в неограниченном количестве. Необходимо вернуть новый числовой список содержащий только уникальные числа.
+
+    public static List<Integer> findUnique(ArrayList arrayList) {
+        List<Integer> listUnique = new ArrayList<>();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if(!listUnique.contains(arrayList.get(i))) {
+                listUnique.add((Integer) arrayList.get(i));
+            }
+        }
+        return listUnique;
+    }
 //
 //   4. ** Создать метод calcOccurance, принимающий на вход строковый список в качестве параметра.
 //   Список заполнен произвольными словами 10-20 штук, которые могут повторяться в неограниченном количестве.
@@ -42,6 +60,14 @@ public class Main {
 //    bird: 2
 //    fox: 1
 //    cat: 1
+
+    public static void calcOccurrence(ArrayList arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.print(arrayList.get(i) + ": ");
+            System.out.println(countOccurrence(arrayList, (String) arrayList.get(i)));
+        }
+        System.out.println();
+    }
 //
 //   4. *** Создать метод findOccurance, принимающий на вход строковый список в качестве параметра.
 //   Список заполнен произвольными словами 10-20 штук, которые могут повторяться в неограниченном количестве.
@@ -53,7 +79,17 @@ public class Main {
 //    {name: "bird", occurrence: 2},
 //    {name: "fox", occurrence: 1},
 //    {name: "cat", occurrence: 1}
-//]
+//    ]
+//
 
+    public static void findOccurrence(ArrayList arrayList) {
+        HashMap<String, String> myHashMap = new HashMap<String, String>();
+        Dictionary dictionary = null;
+        for (int i = 0; i < arrayList.size(); i++) {
+            dictionary.put("name:", (String) arrayList.get(i));
+            dictionary.put("occurrence:", String.valueOf(countOccurrence(arrayList, (String) arrayList.get(i))));
+        }
+        System.out.println(dictionary);
 
+    }
 }
