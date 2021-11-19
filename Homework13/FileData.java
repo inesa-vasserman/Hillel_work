7ffe874889c3da8d02e9aa1de43377d19e337221
inesa-vasserman <inna.kiwi.a@gmail.com>
2021-11-19 22:29:26 +0200

    Homework13. First step

---
+++ Homework13/FileData.java
@@ -0,0 +1,10 @@
+package Homework13;
+
+public class FileData {
+
+    //            1. Создать класс FileData. Класс представляет конкретный файл и состоит из: имя файла, размер в байтах, путь к файлу.
+
+    private String name;
+    private int size;
+    private String path;
+}
---
+++ Homework13/Main.java
@@ -0,0 +1,12 @@
+package Homework13;
+
+public class Main {
+
+//    0. Создать класс FileNavigator. Класс предназначен для хранения списка файлов, располагаемые по концертному пути.
+//            Например: /path/to/file -> [files.txt, firstApp.java]
+//
+//
+
+
+
+}
