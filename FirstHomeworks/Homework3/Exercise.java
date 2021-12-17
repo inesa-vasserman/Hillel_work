70d49f9ab4aee206f3f94ba788fb2c67c66c6d74
inesa-vasserman <inna.kiwi.a@gmail.com>
2021-12-17 22:27:11 +0200

    Homework1. Work with files

--- FirstHomeworks/Homework3/Exercise.java
+++ FirstHomeworks/Homework3/Exercise.java
@@ -5,17 +5,15 @@ import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
-    public static int[] randomArr = new int[10];
-    public static int average = 0;
-    public static String newString = "";
-    public static double result;
-    public static int sizeHorizontal = 0;
-    public static int sizeVertical = 0;
+public class Exercise {
+    public int[] randomArr = new int[10];
+    public int average = 0;
+    public String newString = "";
+    public double result;
+    public int sizeHorizontal = 0;
+    public int sizeVertical = 0;
 
     public static void main(String[] args) {
-        System.out.println(checkArray("logger.txt", "test"));
-
 
         // Вывести ряд чисел от 1 до 100 через пробел, с изменениями
         //- если число кратно 3 то вместо него вывести Hello
@@ -25,17 +23,17 @@ public class Main {
         //- если число кратно и 3 и 5 то вместо него вывести HelloWorld
 
 
-//        for (int i = 1; i <= 100; i++) {
-//            if (i % 3 == 0 && i % 5 != 0) {
-//                System.out.print("Hello ");
-//            } else if (i % 3 != 0 && i % 5 == 0) {
-//                System.out.print("World ");
-//            } else if (i % 3 == 0 && i % 5 == 0) {
-//                System.out.print("HelloWorld ");
-//            } else {
-//                System.out.print(i + " ");
-//            }
-//        }
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.print("Hello ");
+            } else if (i % 3 != 0 && i % 5 == 0) {
+                System.out.print("World ");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("HelloWorld ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
 
 
     }
@@ -43,7 +41,7 @@ public class Main {
     // Написать метод который вернет String в которой будут в одну строку числа от 1 до 30. Каждое число при этом взять в круглые скобки ()
     // FIXED
 
-    public static String makeLineNumber() {
+    public String makeLineNumber() {
         String res = "";
         StringBuilder stringLine = new StringBuilder();
         for (int i = 1; i < 31; i++) {
@@ -56,7 +54,7 @@ public class Main {
     // Заполнить массив из 10 элементов случайными целыми числами от 10(включительно) до 20(включительно). Вывести среднее арифметическое.
     // FIXED
 
-    public static double randomizeArray() {
+    public double randomizeArray() {
         for (int i = 0; i < 10; i++) {
             Random random = new Random();
             int randNum = random.nextInt(10 + 1) + 10;
@@ -70,15 +68,13 @@ public class Main {
 
     // Написать метод который убирает лишние пробелы в строке и возвращает строку.
 
-    public static String deleteSpace(String str) {
+    public String deleteSpace(String str) {
         char[] toCharArray = str.trim().toCharArray();
         for (int i = 0; i < toCharArray.length; i++) {
             char space = ' ';
             if (toCharArray[i] == space && toCharArray[i + 1] == space) {
-                continue;
-
             } else {
-                newString += toCharArray[i];
+                newString = newString + toCharArray[i];
             }
 
         }
@@ -92,16 +88,22 @@ public class Main {
     // Массив загрузить из текстового файла
 
 
-    public static boolean checkArray(String filename, String strTest) {
+    public boolean checkArray(String filename, String strTest) {
         char[][] args = load(filename);
         char[] stringToArray = strTest.toCharArray();
         int countLine = args.length;
         int countCol = args[0].length;
         int counter = 0;
-        for (int i = 0; i < countLine; i++) {
-            for (int j = 0; j < countCol; j++) {
-                if (args[i][j] == stringToArray[counter]) {
+
+        for (char[] arg : args) {
+            for (char c : arg) {
+                if (c == stringToArray[counter]) {
                     counter++;
+                    for (char value : arg) {
+                        if (value == stringToArray[counter]) {
+                            counter++;
+                        }
+                    }
                     if (counter == stringToArray.length) {
                         return true;
                     }
@@ -112,8 +114,8 @@ public class Main {
         }
 
         for (int i = 0; i < countCol; i++) {
-            for (int j = 0; j < countLine; j++) {
-                if (args[j][i] == stringToArray[counter]) {
+            for (char[] arg : args) {
+                if (arg[i] == stringToArray[counter]) {
                     counter++;
                     if (counter == stringToArray.length) {
                         return true;
@@ -127,7 +129,7 @@ public class Main {
         return false;
     }
 
-    public static char[][] load(String filename) {
+    public char[][] load(String filename) {
         countSize(filename);
         char[][] args = new char[sizeVertical + 1][sizeHorizontal];
         try {
@@ -136,7 +138,9 @@ public class Main {
             for (int i = 0; i < args.length; i++) {
                 for (int j = 0; j < args[i].length; j++) {
                     String data = myReader.next();
-                    args[i][j] = data.charAt(0);
+                    if (!data.equals(" ")) {
+                        args[i][j] = data.charAt(0);
+                    }
                 }
             }
 
@@ -148,7 +152,7 @@ public class Main {
         return args;
     }
 
-    public static void countSize(String filename) {
+    public void countSize(String filename) {
         String counterLine = "";
         try {
             File myObj = new File(filename);
@@ -157,7 +161,7 @@ public class Main {
                 sizeVertical++;
 
                 if (sizeVertical == 1) {
-                    counterLine = myReader.nextLine().replace(" ", "");
+                    counterLine = myReader.nextLine().replaceAll(" ", "");
                 }
                 myReader.nextLine();
             }
